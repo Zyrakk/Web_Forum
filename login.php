908a25c4cb3c28ddc3705607b4455c37a3e2592a
Zyrakk <sykblade1@gmail.com>
2024-02-21 01:27:36 +0100

    Home_V2.0

--- login.php
+++ login.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="website, project, tutorial">
+    <meta name="author" content="Stefan Contreras">
     <!--Hojas de Estilo CSS y Favicon-->
     <link rel="stylesheet" href="./assets/styles_compartido.css">
     <link rel="stylesheet" href="./assets/styles_login.css">
