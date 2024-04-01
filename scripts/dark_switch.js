8ccb1da22035ec0567f94d7b29a8ef7e1f4183cc
Zyrakk <sykblade1@gmail.com>
2024-04-01 09:01:24 +0200

    Update dark_switch.js

--- scripts/dark_switch.js
+++ scripts/dark_switch.js
@@ -1,6 +1,27 @@
 const switchButton = document.getElementById('switch');
- 
+
+if (localStorage.getItem("tema") === null) {
+    var tema = 'light';
+    document.body.classList.add('light'); // Add initial theme class to the HTML body
+} else {
+    var tema = localStorage.getItem("tema");
+    document.body.classList.add(tema); // Add initial theme class to the HTML body
+}
+
+function changeTheme() {
+    if (tema === 'light') {
+        document.body.classList.remove('light');
+        document.body.classList.add('dark');
+        tema = 'dark';
+    } else {
+        document.body.classList.remove('dark');
+        document.body.classList.add('light');
+        tema = 'light';
+    }
+}
+
 switchButton.addEventListener('click', () => {
-    document.body.classList.toggle('dark'); //toggle the HTML body the class 'dark'
-    switchButton.classList.toggle('active');//toggle the HTML button with the id='switch' with the class 'active'
+    changeTheme();
+    switchButton.classList.toggle('active');
+    localStorage.setItem("tema", tema);
 });
