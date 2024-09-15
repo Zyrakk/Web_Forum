bd76aa3d9ee19476d68589a6ecf299ce55cb14e4
Zyrakk <150195749+Zyrakk@users.noreply.github.com>
2024-09-16 01:21:21 +0200

    Added maintenance page

---
+++ php/maintenance_check.php
@@ -0,0 +1,14 @@
+<?php
+// Configurar la fecha y hora de finalización del mantenimiento
+$maintenance_end_time = strtotime('2024-09-18 18:00:00 Europe/Madrid');
+
+// Obtener la hora actual
+$current_time = time();
+
+// Verificar si el mantenimiento ha terminado
+if ($current_time < $maintenance_end_time) {
+    // Redirigir a la página de mantenimiento si el mantenimiento no ha terminado
+    header('Location: /mantenimiento.html');
+    exit;
+}
+?>
--- signup.php
+++ signup.php
@@ -1,3 +1,7 @@
+<?php
+    require('php/mantenimiento.php');
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
