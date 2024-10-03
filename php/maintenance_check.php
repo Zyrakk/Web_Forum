4de0ab493e3ec50ec9e450e85f80514190fb10f6
Zyrakk <sykblade1@gmail.com>
2024-10-03 12:38:10 +0200

    Maintenance config

--- php/maintenance_check.php
+++ php/maintenance_check.php
@@ -2,7 +2,6 @@
 // Configurar la zona horaria
 date_default_timezone_set('Europe/Madrid');
 
-// Incluir la configuración
 $config = include('../config.php');
 
 // Obtener la hora actual
@@ -11,10 +10,6 @@ $current_time = new DateTime();
 // Convertir las fechas de mantenimiento a objetos DateTime
 $maintenance_start = new DateTime($config['maintenance_start']);
 $maintenance_end = new DateTime($config['maintenance_end']);
-
-// Debug: Mostrar las fechas para verificar que son correctas
-echo "Inicio del mantenimiento: " . $maintenance_start->format('Y-m-d H:i:s') . "<br>";
-echo "Fin del mantenimiento: " . $maintenance_end->format('Y-m-d H:i:s') . "<br>";
 
 // Verificar si estamos en el período de mantenimiento
 if ($current_time >= $maintenance_start && $current_time <= $maintenance_end) {
