0fc58675d92cf231b77319221891cc1339fc580a
Zyrakk <150195749+Zyrakk@users.noreply.github.com>
2025-02-11 21:07:46 +0100

    Portfolio added project 2

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -5,6 +5,13 @@ async function fetchGitHubRepo() {
 }
 fetchGitHubRepo();
 
+async function fetchGitHubRepo2() {
+    const response = await fetch("https://api.github.com/repos/Zyrakk/py-to-uf2");
+    const data = await response.json();
+    document.getElementById("repo-description_2").innerText = data.description || "Sin descripción disponible.";
+}
+fetchGitHubRepo2();
+
 document.addEventListener("DOMContentLoaded", function () {
     const menuToggle = document.querySelector(".menu-toggle");
     const navContainer = document.querySelector(".nav-container");
@@ -15,4 +22,3 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 });
-
